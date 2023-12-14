f72b5478d91b9744bea18e9ec4a7fb30ed692746
JustFog36 <jfogle36@gmail.com>
2023-12-13 22:18:26 -0500

    please deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,16 @@
-import ReactRefresh from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [ReactRefresh()],
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      // Your proxy configurations if needed
+    },
+  },
+  build: {
+    outDir: 'dist', // Output directory for production build
+  },
+});
